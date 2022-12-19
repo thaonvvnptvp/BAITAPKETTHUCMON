f8697bd52ba35f622ffd8bddd246ed4b0fd784ed
thaonvvnptvp <thaonv.vnptvp@gmail.com>
2022-12-19 08:33:03 +0700

    commit

--- src/main/java/TransactionInput.java
+++
@@ -1,8 +0,0 @@
-public class TransactionInput {
-    public String transactionOutputId;
-    public TransactionOutput UTXO; // Unspend Transaction Output
-
-    public TransactionInput(String transactionOutputId) {
-        this.transactionOutputId = transactionOutputId;
-    }
-}
